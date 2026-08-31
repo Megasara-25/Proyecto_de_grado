73b62e1bdf74396049a57dcc82c58b53f21abef0
Sara Valeria Patarroyo Alba <sara.patarroyoa@uniagustiniana.edu.co>
2026-08-31 17:12:18 -0500

    Implementa registro de entregas y consulta del reciclador

--- src/pages/Ciudadano.jsx
+++ src/pages/Ciudadano.jsx
@@ -5,6 +5,12 @@ import {supabase} from '../lib/supabase'
 function Ciudadano({ perfil, cerrarSesion }) {
   const [materiales, setMateriales] = useState([])
   const [materialSeleccionado, setMaterialSeleccionado] = useState (null)
+  const [recicladores, setRecicladores] = useState ([])
+  const [recicladorSeleccionado, setRecicladorSeleccionado] = useState(null)
+  const [cantidad, setCantidad] = useState('')
+  const [ciudadanoId, setCiudadanoId] = useState(null)
+  const [mensaje, setMensaje] = useState ('')
+
   const cargarMateriales = async () => {
 
     const {data, error} = await supabase
@@ -15,12 +21,85 @@ function Ciudadano({ perfil, cerrarSesion }) {
       console.error('Error al cargar materiales:', error)
       return
     }
+
     setMateriales(data)
     
   }
 
+  const cargarCiudadano = async () => {
+    const {data, error} = await supabase
+    .from('ciudadano')
+    .select('id')
+    .eq('usuarioid', perfil.id)
+    .single()
+  
+  if(error){
+    console.error('Error al cargar ciudadano:', error)
+    return
+  }
+
+  setCiudadanoId(data.id)
+
+  }
+
+  const registrarEntrega = async () => {
+    if(!materialSeleccionado || !recicladorSeleccionado || !cantidad) {
+      alert('Debes seleccionar material, centro y cantidad')
+      return
+    }
+
+    const {data, error} = await supabase
+    .from('entrega')
+    .insert([
+      {
+        cantidad: Number(cantidad),
+        ciudadanoid : ciudadanoId,
+        recicladorid: recicladorSeleccionado.id,
+        materialid: materialSeleccionado.id,
+      }
+    ])
+    . select()
+
+  if(error){
+    console.error('Error al registrar entregas:', error)
+    return
+  }
+
+  console.log('Entrega registrada:', data)
+  
+  setMensaje('Entrega registrada correctamente')
+  setCantidad('')
+  }
+
+  const cargarRecicladores = async (materialId) => {
+
+    const {data,error} = await supabase
+    .from ('material_reciclador')
+    .select(`
+      recicladorid,
+      reciclador(
+        id,
+        nombre_centro, 
+        direccion,
+        ciudad,
+        latitud,
+        longitud
+      )
+    `)
+    .eq('materialid', materialId)
+
+  if (error){
+    console.error('Error al cargar recicladores:', error)
+    return
+  }
+
+  setRecicladores(data)
+  console.log('Recicladores encontrados:', data)
+  }
+
   useEffect(()=>{
     cargarMateriales()
+    cargarCiudadano()
   },[])
 
   return (
@@ -41,7 +120,12 @@ function Ciudadano({ perfil, cerrarSesion }) {
         {materiales.map((material) =>(
           <div 
             key = {material.id}
-            onClick = {() => setMaterialSeleccionado(material)}
+            onClick = {() => {
+              setMaterialSeleccionado(material)
+              setRecicladorSeleccionado(null)
+              setCantidad('')
+              cargarRecicladores(material.id)
+            }}
           >
               <h3>{material.nombre}</h3>
 
@@ -56,7 +140,48 @@ function Ciudadano({ perfil, cerrarSesion }) {
             Material seleccionado: <strong>{materialSeleccionado.nombre}</strong>
           </p>
         )}
+        {recicladores.map((item)=> (
+          <div 
+            key={item.reciclador.id}
+            onClick ={()=> setRecicladorSeleccionado(item.reciclador)}
+          >
+            <h3>{item.reciclador.nombre_centro}</h3>
 
+            <p> 
+              Dirección: {item.reciclador.direccion}
+            </p> 
+            <p>
+              Ciudad: {item.reciclador.ciudad}
+            </p>
+            </div>
+        ))}
+
+        {recicladorSeleccionado && (
+          <p>
+            centro seleccionado: {' '}
+            <strong>{recicladorSeleccionado.nombre_centro}</strong>
+          </p>
+        )}
+
+        {recicladorSeleccionado && (
+          <div>
+            <label>Cantidad en kilogramos</label>
+            <input
+            type="number"
+            min="0.1"
+            step="0.1"
+            placeholder="Ejemplo: 2.5"
+            value ={cantidad}
+            onChange={(e) => setCantidad(e.target.value)}
+            />
+            <button onClick = {registrarEntrega}>
+              registrar entrega
+            </button>
+            {mensaje&&(
+              <p>{mensaje}</p>
+            )}
+          </div>
+        )}
         <button onClick={cerrarSesion}>
           Cerrar sesión
         </button>
--- src/pages/Reciclador.jsx
+++ src/pages/Reciclador.jsx
@@ -1,4 +1,59 @@
+import { useEffect, useState } from "react"
+import { supabase } from '../lib/supabase'
+
 function Reciclador({ perfil, cerrarSesion }) {
+  const [recicladorId, setRecicladorId] = useState(null)
+  const [entregas, setEntregas] = useState ([])
+
+  const cargarReciclador = async () => {
+    
+    const {data, error} = await supabase
+    .from('reciclador')
+    .select('id')
+    .eq('usuarioid', perfil.id)
+    .single()
+
+  if (error){
+    console.error('Eror al cargar reciclador:', error)
+    return
+  }
+
+  setRecicladorId(data.id)
+  cargarEntregas(data.id)
+  }
+
+  const cargarEntregas = async(idReciclador)=>{
+    
+    console.log('ID reciclador usado para buscar entregas:', idReciclador)
+
+    const {data, error} = await supabase
+    .from('entrega')
+    .select(`
+      id,
+      cantidad,
+      puntos_obtenidos,
+      estado,
+      fecha,
+      material(
+      nombre
+      )
+    `)
+    .eq('recicladorid', idReciclador)
+    .eq('estado', 'Pendiente')
+
+  if (error){
+    console.error('Error al cargar entregas:', error)
+    return
+  }
+  
+  setEntregas(data)
+  console.log('Entregas pendientes:', data)
+  }
+
+  useEffect(() =>{
+    cargarReciclador()
+  },[])
+
   return (
     <div className="contenedor">
       <div className="tarjeta">
@@ -8,6 +63,25 @@ function Reciclador({ perfil, cerrarSesion }) {
         <p>
           Bienvenido, <strong>{perfil.nombre}</strong>
         </p>
+        <h2>Entregas pendientes</h2>
+
+        {entregas.map((entrega)=>(
+        <div key ={entrega.id}>
+          <h3>{entrega.material.nombre}</h3> 
+
+          <p>
+            Cantidad: {entrega.cantidad} kg
+          </p>
+
+          <p>
+            Ecopuntos: {entrega.puntos_obtenidos}
+          </p>
+
+          <p>
+            Estado: {entrega.estado}
+          </p>
+          </div>
+        ))}
 
         <button onClick={cerrarSesion}>
           Cerrar sesión
